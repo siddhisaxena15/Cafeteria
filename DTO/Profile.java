94b275cad8c8d297b8a57f689c62238fc55a7627
Siddhi Saxena <siddhi.saxena@intimetec.com>
2024-06-21 20:30:10 +0530

    DTO

---
+++ DTO/Profile.java
@@ -0,0 +1,49 @@
+package com.DTO;
+
+public class Profile {
+    private String employeeId;
+    private String diet;
+    private String spice;
+    private String  cuisineType;
+    private String sweetTooth;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getDiet() {
+        return diet;
+    }
+
+    public void setDiet(String diet) {
+        this.diet = diet;
+    }
+
+    public String getSpice() {
+        return spice;
+    }
+
+    public void setSpice(String spice) {
+        this.spice = spice;
+    }
+
+    public String getCuisineType () {
+        return  cuisineType ;
+    }
+
+    public void setCuisineType (String preference) {
+        this.cuisineType  =  cuisineType ;
+    }
+
+    public String getSweetTooth() {
+        return sweetTooth;
+    }
+
+    public void setSweetTooth(String sweetTooth) {
+        this.sweetTooth = sweetTooth;
+    }
+}
