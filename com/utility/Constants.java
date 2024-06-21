699962fd44c2c78b4ff944eebd64bfb2d1c664ef
Siddhi Saxena <siddhi.saxena@intimetec.com>
2024-06-21 20:30:10 +0530

    Second

---
+++ com/utility/Constants.java
@@ -0,0 +1,36 @@
+package com.utility;
+
+public class Constants {
+    public static final String DISCARD_MENU_REQUEST = "DISCARD_MENU_REQUEST";
+    public static final String DISCARD_MENU_RESPONSE = "DISCARD_MENU_RESPONSE";
+    public static final String DISCARD_MENU_ACTION_VALID = "DISCARD_MENU_ACTION;VALID";
+    public static final String FEEDBACK_DETAILS_REQUEST = "FEEDBACK_DETAILS_REQUEST";
+    public static final String VIEW_NOTIFICATIONS_REQUEST = "VIEW_NOTIFICATIONS_REQUEST";
+    public static final String VIEW_NOTIFICATIONS_RESPONSE = "VIEW_NOTIFICATIONS_RESPONSE";
+    public static final String GIVE_FEEDBACK_RESPONSE = "GIVE_FEEDBACK_RESPONSE";
+    public static final String GIVE_FEEDBACK_REQUEST = "GIVE_FEEDBACK_REQUEST;";
+    public static final String VOTE_RECOMMENDATION_RESPONSE = "VOTE_RECOMMENDATION_RESPONSE";
+    public static final String VOTE_RECOMMENDATION_REQUEST =  "VOTE_RECOMMENDATION_REQUEST;";
+
+    public static final String VIEW_RECOMMENDATIONS_RESPONSE = "VIEW_RECOMMENDATIONS_RESPONSE";
+    public static final String VIEW_CHEF_RECOMMENDATIONS = "VIEW_CHEF_RECOMMENDATIONS";
+    public static final  String VIEW_MENU_REQUEST = "VIEW_MENU_REQUEST";
+    public static final  String VIEW_TOP_RECOMMENDATIONS = "VIEW_TOP_RECOMMENDATIONS;";
+    public static final String VIEW_VOTED_REPORT = "VIEW_VOTED_REPORT";
+    public static final String ROLLOUT_NEXT_DAY_MENU_REQUEST = "ROLLOUT_NEXT_DAY_MENU_REQUEST;";
+    public static final String ROLLOUT_NEXT_DAY_MENU_RESPONSE = "ROLLOUT_NEXT_DAY_MENU_RESPONSE";
+    public static final String ADD_MENU_REQUEST = "ADD_MENU_REQUEST;";
+    public static final String UPDATE_MENU_REQUEST = "UPDATE_MENU_REQUEST;";
+    public static final String DISCARD_FEEDBACK_DETAILS_REQUEST_FOR_ADMIN_CHEF = "DISCARD_FEEDBACK_DETAILS_REQUEST_FOR_ADMIN_CHEF;";
+    public static final String DISCARD_FEEDBACK_DETAILS_REQUEST_FOR_EMPLOYEE = "DISCARD_FEEDBACK_DETAILS_REQUEST_FOR_EMPLOYEE;";
+    public static final String SUCCESS = "SUCCESS";
+    public static final String DELETE_MENU_REQUEST = "DELETE_MENU_REQUEST;";
+    public static final String USER_SESSION_REQUEST = "USER_SESSION_REQUEST;";
+    public static final String LOGIN_REQUEST = "LOGIN_REQUEST;";
+    public static final String LOGIN_RESPONSE = "LOGIN_RESPONSE";
+    public static final String ERROR_PREFIX = "ERROR: ";
+    public static final String ACTION_REMOVE = "1";
+    public static final String ACTION_DETAILS = "2";
+    public static final String UPDATE_PROFILE_REQUEST = "UPDATE_PROFILE_REQUEST;";
+    public static final String UPDATE_PROFILE_RESPONSE = "UPDATE_PROFILE_RESPONSE;";
+}
