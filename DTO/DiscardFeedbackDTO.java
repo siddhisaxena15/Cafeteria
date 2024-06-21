fe3a3d433d170e293f7c366288bf202b897a41cd
Siddhi Saxena <siddhi.saxena@intimetec.com>
2024-06-21 20:30:10 +0530

    DTO

---
+++ DTO/DiscardFeedbackDTO.java
@@ -0,0 +1,23 @@
+package com.DTO;
+
+public class DiscardFeedbackDTO {
+    private int menuId;
+    private String question;
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+}
---
+++ DTO/UserDTO.java
@@ -0,0 +1,59 @@
+package com.DTO;
+
+public class UserDTO {
+
+    private int userId;
+    private String employeeId;
+    private String name;
+    private String password;
+    private int roleId;
+    private String roleName;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
