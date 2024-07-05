495daf88cf0fe501504e9624916067387ee5785b
siddhisaxena15 <siddhisaxena1510@gmail.com>
2024-07-05 12:00:00 +0530

    service classes

---
+++ utility/Constants.java
@@ -0,0 +1,20 @@
+package com.utility;
+
+public class Constants {
+    public static final String LOGIN_REQUEST = "LOGIN_REQUEST";
+    public static final String ADD_MENU_REQUEST = "ADD_MENU_REQUEST";
+    public static final String UPDATE_MENU_REQUEST = "UPDATE_MENU_REQUEST";
+    public static final String DELETE_MENU_REQUEST = "DELETE_MENU_REQUEST";
+    public static final String VIEW_MENU_REQUEST = "VIEW_MENU_REQUEST";
+    public static final String VIEW_TOP_RECOMMENDATIONS = "VIEW_TOP_RECOMMENDATIONS";
+    public static final String VIEW_CHEF_RECOMMENDATIONS = "VIEW_CHEF_RECOMMENDATIONS";
+    public static final String VOTE_RECOMMENDATION_REQUEST = "VOTE_RECOMMENDATION_REQUEST";
+    public static final String VIEW_VOTED_REPORT = "VIEW_VOTED_REPORT";
+    public static final String ROLLOUT_NEXT_DAY_MENU_REQUEST = "ROLLOUT_NEXT_DAY_MENU_REQUEST";
+    public static final String GIVE_FEEDBACK_REQUEST = "GIVE_FEEDBACK_REQUEST";
+    public static final String USER_SESSION_REQUEST = "USER_SESSION_REQUEST";
+    public static final String VIEW_NOTIFICATIONS_REQUEST = "VIEW_NOTIFICATIONS_REQUEST";
+    public static final String DISCARD_MENU_REQUEST = "DISCARD_MENU_REQUEST";
+    public static final String DISCARD_FEEDBACK_DETAILS_REQUEST_FOR_EMPLOYEE = "DISCARD_FEEDBACK_DETAILS_REQUEST_FOR_EMPLOYEE";
+    public static final String UPDATE_PROFILE_REQUEST = "UPDATE_PROFILE_REQUEST";
+}
